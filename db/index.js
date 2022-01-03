b75844003c28b8c37408f2bbe60754cbbef8897f
KHenaghen <khenaghen@gmail.com>
2022-01-03 11:19:40 -0800

    Part 2 up to Collecting Common Middleware

--- db/index.js
+++ db/index.js
@@ -1,6 +1,23 @@
 const { Client } = require('pg');
+const PORT = 3000;
+const express = require('express');
+const server = express();
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
 
 const client = new Client('postgres://localhost:5432/juicebox-dev');
+
+server.listen(PORT, () => {
+    console.log('The server is up on port', PORT)
+});
+
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+  
+    next();
+  });
 
 async function getAllUsers() {
     try {
